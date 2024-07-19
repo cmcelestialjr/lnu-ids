7bc6d90e9ed59a7e8681e2a20182ceea7a8c0e3e
cesar <cesar.celestialjr@lnu.edu.ph>
2024-07-19 14:45:55 +0800

    update

--- public/assets/js/hrims/payroll/monitoring/partTimeViewDtr.js
+++ public/assets/js/hrims/payroll/monitoring/partTimeViewDtr.js
@@ -1,20 +1,20 @@
 
 viewDtr($('#ptOptions #dtr'));
 function viewDtr(thisBtn){
-    var sy = $('#partTimeSY option:selected').val();
-    var url = base_url+'/hrims/payroll/monitoring/partTime/viewDtr';
+    var sy = $('#overLoadSY option:selected').val();
+    var url = base_url+'/hrims/payroll/monitoring/overLoad/update';
     var modal = 'default';
     var modal_size = 'modal-sm';
-
-    var form_data = $('#ptOptions').serializeArray();
-
-    form_data.push({ name: 'url', value: url });
-    form_data.push({ name: 'modal', value: modal });
-    form_data.push({ name: 'modal_size', value: modal_size });
-    form_data.push({ name: 'static', value: '' });
-    form_data.push({ name: 'w_table', value: 'wo' });
-
-    var serialized_data = $.param(form_data);
-
-    loadDivwLoader(serialized_data,thisBtn);
+    var form_data = {
+        url:url,
+        modal:modal,
+        modal_size:modal_size,
+        static:'',
+        w_table:'wo',
+        id:id,
+        option_id:option_id,
+        work_id:work_id,
+        sy:sy
+    };
+    loadDivwLoader(form_data,thisBtn);
 }
